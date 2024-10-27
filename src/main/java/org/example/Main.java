409eb5e99ea7ae23e6ec254f1d43a61d9d0b8ae0
Mayur Kawle <mayur@futurewise.co.in>
2024-10-27 15:03:31 +0530

    this commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hey This is the Java basic project");
+        System.out.println("I will make the deployments by Jenkis later");
+
+    }
+}
